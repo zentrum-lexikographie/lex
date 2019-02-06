fce3176dbecddedc95152262da635b1ad8c19838
Gregor Middell <gregor.middell@bbaw.de>
2019-02-06 15:37:50 +0100

    Java sources compile

--- project.clj
+++ project.clj
@@ -3,9 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.swinglabs.swingx/swingx-all "1.6.5-1"]
+                 [org.exist-db/existdb-core "2.2"]]
+  :repositories [["exist" "https://raw.github.com/eXist-db/mvn-repo/master"]
+                 ["oxygen" {:url "https://www.oxygenxml.com/maven"
+                            :snapshots true}]]
   :main ^:skip-aot dwdsox.core
   :java-source-paths ["src"]
   :target-path "target/%s"
-  :plugins [[lein-pprint "1.2.0"]]
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :provided {:dependencies [[com.oxygenxml/oxygen-sdk "20.1.0.1"]]}})
