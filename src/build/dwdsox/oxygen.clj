9c2ebdbf851a55fc0573080d3fb9baca5d80b0cd
Gregor Middell <gregor.middell@bbaw.de>
2019-02-07 15:27:09 +0100

    Try to detect $OXYGEN_HOME

--- src/build/dwdsox/oxygen.clj
+++ src/build/dwdsox/oxygen.clj
@@ -1,16 +1,25 @@
 (ns dwdsox.oxygen
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]
+            [me.raynes.fs :as fs])
+  (:import [java.io File]))
+
+(def oxygen-home (->>
+                  (concat [(File. (or (System/getenv "OXYGEN_HOME") "./oxygen"))]
+                           (fs/find-files "/usr/local" #"Oxygen XML Editor [0-9\.]+"))
+                  (filter #(.isDirectory %))
+                  (map #(.getAbsolutePath %))
+                  (first)))
 
 (defn -main [& args]
-  (let [home (System/getenv "OXYGEN_HOME")]
-    (doto (ProcessBuilder.
-           ["java"
-            "-Dcom.oxygenxml.editor.plugins.dir=target/oxygen/plugins"
-            "-Dcom.oxygenxml.app.descriptor=ro.sync.exml.EditorFrameDescriptor"
-            "-cp" (string/join ":" [(str home "/lib/oxygen.jar")
-                                    (str home "/classes")
-                                    home])
-            "ro.sync.exml.Oxygen"
-            "target/oxygen/test-project/project.xpr"])
-      (.inheritIO)
-      (.start))))
+  (let [pb (ProcessBuilder.
+            ["java"
+             "-Dcom.oxygenxml.editor.plugins.dir=target/oxygen/plugins"
+             "-Dcom.oxygenxml.app.descriptor=ro.sync.exml.EditorFrameDescriptor"
+             "-cp" (string/join ":" [(str oxygen-home "/lib/oxygen.jar")
+                                     (str oxygen-home "/classes")
+                                     oxygen-home])
+             "ro.sync.exml.Oxygen"
+             "target/oxygen/test-project/project.xpr"])]
+    (.. pb (environment) (put "OXYGEN_HOME" oxygen-home))
+    (doto pb (.inheritIO) (.start))))
