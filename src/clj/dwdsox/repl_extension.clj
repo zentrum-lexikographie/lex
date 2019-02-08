56803a37a4302cf8d281fcfe9c57fdb9fd91708f
Gregor Middell <gregor.middell@bbaw.de>
2019-02-08 17:14:55 +0100

    Remove test code from REPL extension

--- src/clj/dwdsox/repl_extension.clj
+++ src/clj/dwdsox/repl_extension.clj
@@ -8,26 +8,18 @@
    :state state
    :init init))
 
-(def sample-xquery (-> "dwdsox/xquery/hello.xq" io/resource slurp))
-
-(defonce ws-access (atom nil))
-
 (defn -init []
   [[] (ref {:server (atom nil)})])
 
 (defn -applicationStarted [this app-ws-access]
   (when-let [port (-> (System/getProperty "dwdsox.repl.port") Integer/parseInt)]
     (let [{:keys [server]} @(.state this)]
-      (reset! server (repl/start-server :port port)))
-    (reset! ws-access app-ws-access))
-  (println (db/query sample-xquery))
-  (println (db/get "indexedvalues.xml")))
+      (reset! server (repl/start-server :port port)))))
 
 (defn -applicationClosing [this]
   (let [{:keys [server]} @(.state this)]
     (when-not (nil? @server)
       (@server)
       (reset! server nil)))
-  (reset! ws-access nil)
   true)
 
