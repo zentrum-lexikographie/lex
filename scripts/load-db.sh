04d4cd8b7603ca72af7085803e8725749f1b438d
Gregor Middell <gregor.middell@bbaw.de>
2019-02-14 12:07:39 +0100

    Set up users/groups in test db; bind-mount production backup dump

--- scripts/load-db.sh
+++ scripts/load-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d data ]; then
+if [ ! -f data/backup.properties ]; then
     db_host=spock.dwds.de
 
     backup_file=$(ssh $db_host\
@@ -11,18 +11,24 @@ if [ ! -d data ]; then
 
     scp ${db_host}:${backup_file} $temp_backup_file
 
-    mkdir data
     unzip -W $temp_backup_file\
           -x\
           db/apps/**\
-          db/dwdswb/xquery/collection.xconf\
           db/system/config/db/apps/**\
-          db/system/config/db/dwdswb/**\
-          db/system/security/**\
+          db/system/security/exist/accounts/admin.xml\
+          db/system/security/exist/accounts/eXide.xml\
+          db/system/security/exist/accounts/guest.xml\
+          db/system/security/exist/accounts/monex.xml\
+          db/system/security/exist/groups/eXide.xml\
+          db/system/security/exist/groups/monex.xml\
           db/system/versions/**\
           db/__lost_and_found__/**\
           -d data
 
+    mv data/db/dwdswb/xquery/collection.xconf\
+       data/db/dwdswb/xquery/collection.xconf.bak
+    mv data/db/system/config/db/dwdswb/collection.xconf\
+       data/db/system/config/db/dwdswb/collection.xconf.bak
     rm $temp_backup_file
 fi
 
@@ -30,7 +36,13 @@ docker exec exist\
        java -jar start.jar client --no-gui\
        --xpath "repo:install-and-deploy('http://exist-db.org/xquery/versioning', 'http://demo.exist-db.org/exist/apps/public-repo/modules/find.xql')"
 
-docker cp data exist:/exist/webapp/WEB-INF/data/prod
 docker exec exist\
        java -jar start.jar client --no-gui\
-       --xpath "system:restore('/exist/webapp/WEB-INF/data/prod', '', '')"
+       --xpath "system:restore('/exist/webapp/WEB-INF/data/production', '', '')"
+
+# TODO: upload corrected data/db/system/config/db/dwdswb/collection.xconf.bak
+
+docker exec exist\
+       java -jar start.jar client --no-gui\
+       --xpath "xmldb:reindex('/db/dwdsdb')"
+
