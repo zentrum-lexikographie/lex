6724bc16af6e5ff0a2585fec5c8535556f734e75
Gregor Middell <gregor.middell@bbaw.de>
2019-03-20 15:06:47 +0100

    Remove placeholder unit test

--- server/test/zdl_lex_server/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns zdl-lex-server.core-test
-  (:require [clojure.test :refer :all]
-            [zdl-lex-server.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
