3ca4326dc6c523c2eedfde00d243dd3dd4c2234a
Gregor Middell <gregor.middell@bbaw.de>
2019-04-09 16:54:55 +0200

    Implement article quick search

--- client/src/clj/zdl_lex_client/quicksearch.clj
+++ client/src/clj/zdl_lex_client/quicksearch.clj
@@ -1,32 +1,79 @@
 (ns zdl-lex-client.quicksearch
   (:require [seesaw.core :as ui]
-            [seesaw.options :refer [apply-options]]
+            [seesaw.border :refer [empty-border line-border]]
             [seesaw.swingx :as uix]
             [seesaw.mig :as uim]
             [taoensso.timbre :as timbre]
-            [zdl-lex-client.http :as http])
-  (:import [com.jidesoft.hints AbstractListIntelliHints]))
+            [zdl-lex-client.http :as http]
+            [clojure.string :as str])
+  (:import [javax.swing JLabel]
+           [com.jidesoft.hints AbstractListIntelliHints]))
 
+(defn- suggestion-text [{:keys [suggestion pos type definitions 
+                         status id last-modified]}]
+  (let [suggestion (-> suggestion
+                       (str/replace "<b>" "<u>")
+                       (str/replace "</b>" "</u>"))
+        suggestion (str "<font size=\"+1\">" suggestion "</font>")
 
-(defn form []
+        pos (-> pos first (or "?"))
+        pos (str "&lt;" pos "&gt;")
+
+        source (re-find #"^[^/]+" id)
+        source (str "<font color=\"blue\">[" source "]</font>")
+
+        title (str/join " " (remove nil? [suggestion pos source]))
+
+        subtitle (str/join ", " (remove nil? [type status last-modified]))
+
+        definition (-> definitions first (or "ohne Def."))
+        definition-length (count definition)
+        definition (subs definition 0 (min 40 definition-length))
+        definition (str definition (if (< 40 definition-length) "…" ""))
+        definition (str "<i>" definition "</i>")
+
+        html (str/join "<br>" (remove nil? [title subtitle definition]))]
+
+    (str "<html>" html "</html>")))
+
+(defn- suggestion-color [{:keys [status]}]
+  (cond
+    (= "Red-f" status) :green
+    (= "Red-2" status) :yellow
+    (= "Artikelrumpf" status) :lightgrey
+    (.startsWith status "Lex-") :orange
+    (.endsWith status "-zurückgewiesen") :red
+    :else :white))
+
+(defn- render-suggestion [this {:keys [value index selected? focus?]}]
+  (ui/config! this
+              :text (suggestion-text value)
+              :font "Dialog-12"
+              :border [5 (line-border :color (suggestion-color value)
+                                      :left 5) 5]))
+
+(defn quick-search []
   (let [input (ui/text :columns 40 :font "18" :border 5)]
     (proxy [AbstractListIntelliHints] [input]
       (createList []
         (let [list (proxy-super createList)]
-          (apply-options list [:background :white :font "18" :border 5])
-          list))
+          (ui/config! list
+                      :background :white
+                      :renderer render-suggestion)))
       (updateHints [ctx]
-        (let [suggestions (->> ctx str http/form-suggestions :result)
-              suggestions (map (comp first :forms) suggestions)]
-          (proxy-super setListData (into-array Object suggestions)))
-        true)
+        (let [q (str ctx)
+              suggestions? (< 1 (count q))
+              suggestions (if suggestions? (-> q http/form-suggestions :result))
+              suggestions (or suggestions [])]
+          (proxy-super setListData (into-array Object suggestions))
+          (-> suggestions empty? not)))
       (acceptHint [hint]
-        (proxy-super acceptHint hint)
+        (proxy-super acceptHint (-> hint :suggestion (str/replace #"</?b>" "")))
         (timbre/info hint)))
     input))
 
 (defn -main []
   (ui/invoke-later
-   (-> (ui/frame :title "Metadatensuche" :content (form))
+   (-> (ui/frame :title "Quick Search" :content (quick-search))
        ui/pack!
        ui/show!)))
