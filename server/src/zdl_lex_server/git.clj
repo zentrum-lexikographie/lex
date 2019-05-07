c31e8de34c24eb64fc73d332233c2947d3db657f
Gregor Middell <gregor.middell@bbaw.de>
2019-05-07 17:14:31 +0200

    Parse git status output

--- server/src/zdl_lex_server/git.clj
+++ server/src/zdl_lex_server/git.clj
@@ -1,6 +1,8 @@
 (ns zdl-lex-server.git
   (:require [clojure.java.shell :as sh]
-            [zdl-lex-server.store :as store]))
+            [zdl-lex-server.store :as store]
+            [clojure.string :as str]
+            [me.raynes.fs :as fs]))
 
 (defn git [& args]
   (locking store/git-dir
@@ -10,12 +12,30 @@
           (throw (ex-info (str args) result)))
         result))))
 
-(def status (partial git "status" "--porcelain"))
+(defn- status-text->vec [txt]
+  (as-> txt $
+    (str/trim $)
+    (str/split $ #"\s+" 2)
+    (vector (keyword (first $)) (second $))))
+
+(defn status []
+  (as-> (git "status" "--porcelain") $
+    (:out $)
+    (str/split $ #"[\n\r]+")
+    (remove empty? $)
+    (map status-text->vec $)
+    (filter (comp #{:?? :M :D} first) $)))
+
+(def add (partial git "add"))
+
+(def rm (partial git "rm"))
 
 (defn commit []
   (let [stat (status)]
-    (when-not (empty? (stat :out))
-      (git "add" ".")
+    (when-not (empty? stat)
+      (add ".")
       (git "commit" "-m" "" "--allow-empty-message")
+      (git "fetch" "origin")
+      (git "rebase" "--allow-empty-message" "-s"  "recursive" "-X" "theirs")
       (git "push" "origin"))
     stat))
