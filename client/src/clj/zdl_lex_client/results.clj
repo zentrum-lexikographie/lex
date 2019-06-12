9ac7add909a4a55fcddb7015d15146cb8ce72bfb
Gregor Middell <gregor.middell@bbaw.de>
2019-06-12 15:35:48 +0200

    Refactors zdl-lex-client.results

--- client/src/clj/zdl_lex_client/results.clj
+++ client/src/clj/zdl_lex_client/results.clj
@@ -3,15 +3,19 @@
             [clojure.string :as str]
             [mount.core :refer [defstate]]
             [seesaw.core :as ui]
+            [seesaw.swingx :as uix]
             [tick.alpha.api :as t]
             [zdl-lex-client.article :as article]
             [zdl-lex-client.icon :as icon]
-            [zdl-lex-client.workspace :as workspace])
+            [zdl-lex-client.workspace :as workspace]
+            [taoensso.timbre :as timbre])
   (:import com.jidesoft.swing.JideTabbedPane
            [java.awt.event ComponentEvent MouseEvent]
            java.awt.Point
            [javax.swing JTabbedPane JTable]
-           [javax.swing.table DefaultTableCellRenderer TableColumn]))
+           javax.swing.table.DefaultTableCellRenderer
+           [org.jdesktop.swingx.decorator AbstractHighlighter Highlighter]
+           org.jdesktop.swingx.JXTable$TableAdapter))
 
 (def output (doto (JideTabbedPane. JTabbedPane/BOTTOM)
               (.setShowCloseButtonOnTab true)))
@@ -20,84 +24,77 @@
 
 (def num-result-tabs (comp count result-tabs))
 
+(def ^:private result-table-columns
+  [{:key :form :text "Schreibung"}
+   {:key :pos :text "Wortklasse"}
+   {:key :type :text "Typ"}
+   {:key :last-modified :text "Datum"}
+   {:key :status :text "Status"}
+   {:key :sources :text "Quellen"}
+   {:key :authors :text "Autoren"}])
+
+(defn- open-articles-in [result]
+  (fn [^MouseEvent e]
+    (let [^JTable table (.getSource e)
+          ^Point point (.getPoint e)
+          row (.rowAtPoint table point)
+          clicks (.getClickCount e)]
+      (when (and (<= 0 row) (= 2 clicks))
+        (workspace/open-article (nth result row))))))
+
+(defn- resize-columns [^ComponentEvent e]
+  (let [^JTable table (.getSource e)
+        table-width (ui/width table)]
+    (doseq [c (range (count result-table-columns))
+            :let [column (nth result-table-columns c)
+                  model (.. table (getColumnModel) (getColumn c))]]
+      (condp = (column :key)
+        :form
+        (doto model
+          (.setMinWidth (int (* 0.25 table-width))))
+        model))
+    (ui/repaint! table)))
+
 (defn- result-values [m]
   (->> m vals flatten sort dedupe (str/join ", ")))
 
+(def ^:private result->table-model
+  (partial map #(merge % {:form (some-> % :forms first)
+                          :pos (some-> % :pos first)
+                          :authors (some-> % :authors result-values)
+                          :sources (some-> % :sources result-values)
+                          :color (article/status->color %)})))
+
 (defn result-table [result]
-  (let [result (map-indexed #(assoc %2 :num (inc %1)) result)
-        result (map #(merge % {:form (some-> % :forms first)
-                               :pos (some-> % :pos first)
-                               :authors (some-> % :authors result-values)
-                               :sources (some-> % :sources result-values)
-                               :color (article/status->color %)}) result)
-        columns [{:key :num :text "#" :class clojure.lang.BigInt
-                  :column-config (fn [{:keys [model table-width]}]
-                                   (doto ^TableColumn model
-                                     (.setMinWidth 50)
-                                     (.setMaxWidth (int (* 0.1 table-width)))))}
-                 {:key :form :text "Schreibung"
-                  :renderer (fn [{:keys [component]}]
-                              (ui/config! component :font {:style :bold}))
-                  :column-config (fn [{:keys [model table-width]}]
-                                   (doto ^TableColumn model
-                                     (.setMinWidth (int (* 0.25 table-width)))))}
-                 {:key :pos :text "Wortklasse"}
-                 {:key :type :text "Typ"}
-                 {:key :last-modified :text "Datum"}
-                 {:key :status
-                  :text "Status"
-                  :renderer (fn [{:keys [component value selected? focus?]}]
-                              (let [{:keys [color]} value]
-                                (when-not selected?
-                                  (ui/config! component :background color))))}
-                 {:key :sources :text "Quellen"}
-                 {:key :authors :text "Autoren"}]
-        table (ui/table :class :result :model [:rows result :columns columns])
-        ;; merge column models for customization
-        columns (map-indexed
-                 #(hash-map :column %2
-                            :model (.. table (getColumnModel) (getColumn %1)))
-                 columns)]
+  (let [result (result->table-model result)
+        table (uix/table-x :class :result
+                           :model [:rows result
+                                   :columns result-table-columns]
+                           :listen [:mouse-pressed (open-articles-in result)
+                                    :component-resized resize-columns]
+                           :user-data result)]
     (doto table
-      (.setAutoResizeMode JTable/AUTO_RESIZE_ALL_COLUMNS))
-    (doseq [{:keys [column model]} columns
-            :let [{:keys [renderer]} column]
-            :when renderer]
-      (.setCellRenderer
-       model
-       (proxy [DefaultTableCellRenderer] []
-         (getTableCellRendererComponent [t v selected? focus? row column]
-           (let [component (proxy-super getTableCellRendererComponent
-                                        t v selected? focus? row column)]
-             (renderer {:component component
-                        :value (nth result row)
-                        :selected? selected?
-                        :focus? focus?})
-             component)))))
-    (ui/listen
-     table
-     :mouse-pressed
-     (fn [^MouseEvent e]
-       (let [^JTable table (.getSource e)
-             ^Point point (.getPoint e)
-             row (.rowAtPoint table point)
-             clicks (.getClickCount e)]
-         (when (and (<= 0 row) (= 2 clicks))
-           (workspace/open-article
-            (nth result row)))))
-     :component-resized
-     (fn [^ComponentEvent e]
-       (let [^JTable table (.getSource e)
-             table-width (ui/width table)]
-         (doseq [{:keys [column model]} columns
-                 :let [{:keys [column-config]} column]
-                 :when column-config]
-           (column-config {:column column
-                           :model model
-                           :table table
-                           :table-width table-width}))
-         (ui/repaint! table))))
-    (->> table ui/scrollable)))
+      (.setAutoResizeMode
+       JTable/AUTO_RESIZE_ALL_COLUMNS)
+      (.setHighlighters
+       (into-array
+        Highlighter
+        [(proxy [AbstractHighlighter] []
+           (doHighlight [component ^JXTable$TableAdapter adapter]
+             (let [column (.column adapter)
+                   column (nth result-table-columns column)
+                   row (.row adapter)
+                   row (nth result row)
+                   selected? (.isSelected adapter)]
+               (condp = (:key column)
+                 :form
+                 (ui/config! component :font {:style :bold})
+                 :status
+                 (if-not selected?
+                   (ui/config! component :background (row :color))
+                   component)
+                 component))))])))
+       (->> table ui/scrollable)))
 
 (defn add-result [{:keys [id query total result]}]
   (let [id (keyword id)
