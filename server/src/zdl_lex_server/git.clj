b9f928fec97742e2c439b8ffd64c3d1ba169c8eb
Gregor Middell <gregor.middell@bbaw.de>
2019-08-14 11:35:00 +0200

    Refactors handling of remote changes (git)

--- server/src/zdl_lex_server/git.clj
+++ server/src/zdl_lex_server/git.clj
@@ -7,36 +7,53 @@
             [mount.core :refer [defstate]]
             [taoensso.timbre :as timbre]
             [zdl-lex-server.cron :as cron]
-            [zdl-lex-server.store :as store]))
+            [zdl-lex-server.store :as store]
+            [clojure.string :as str]))
 
-(defn git [& args]
+(defn- format-git-result [git-args {:keys [exit out err]}]
+  (str "\n" (apply str (map (constantly "-") (range 72)))
+       "\n"
+       "\n$ git " (str/join " " git-args)
+       "\n"
+       "\n-> exit code: " exit
+       (if (not-empty out) (str "\n\n" out))
+       (if (not-empty err) (str "\n\n" err))
+       "\n" (apply str (map (constantly "-") (range 72)))))
+
+(defn git-unchecked [& args]
   (locking store/git-dir
     (sh/with-sh-dir store/git-dir
-      (let [result (apply sh/sh (concat ["git"] args))
-            exit-code (result :exit)
-            succeeded (= exit-code 0)]
-        (timbre/log (if succeeded :trace :warn) {:git args :result result})
-        (when-not succeeded (throw (ex-info (str args) result)))
+      (let [result (apply sh/sh (concat ["git"] args))]
+        (timbre/info (format-git-result args result))
         result))))
+
+(defn git [& args]
+  (let [{:keys [exit] :as result} (apply git-unchecked args)]
+    (when-not (= exit 0)
+      (timbre/warn (format-git-result args result))
+      (throw (ex-info (str args) result)))
+    result))
 
 (defn changed-files []
   (locking store/git-dir
     (jgit/with-repo store/git-dir
       (->> (jgit/git-status repo) (vals) (apply union)))))
 
+(defn commit* []
+  (git "add" ".")
+  (git "commit" "-m" "zdl-lex-server"))
+
 (defn commit []
   (locking store/git-dir
     (let [changed-files (changed-files)]
       (when-not (empty? changed-files)
-        (git "add" ".")
-        (git "commit" "-q" "-m" "zdl-lex-server")
+        (commit*)
         changed-files))))
 
-(defn rebase []
+(defn merge-origin []
   (locking store/git-dir
-    (git "fetch" "-q" "origin")
-    (git "rebase" "-q" "-s" "recursive" "-X" "theirs")
-    (git "push" "-q" "origin")))
+    (git "pull" "-s" "recursive" "-X" "ours" "--no-edit")
+    (git "push" "origin")))
 
 (defn- absolute-path [f]
   (->> f (fs/file store/git-dir) fs/absolute fs/normalized))
@@ -55,5 +72,11 @@
   :stop (async/close! changes-provider))
 
 (defstate rebase-scheduler
-  :start (cron/schedule "0 0 * * * ?" "Git rebase" rebase)
+  :start (cron/schedule "0 0 * * * ?" "Git merge" merge-origin)
   :stop (async/close! rebase-scheduler))
+
+(comment
+  (changed-files)
+  (commit)
+  (merge-origin)
+  (println (format-git-result ["status"] (git "status"))))
