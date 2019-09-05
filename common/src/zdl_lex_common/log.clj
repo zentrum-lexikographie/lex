a843847253c758c84878710c7e0d0e63dde74b69
Gregor Middell <gregor.middell@bbaw.de>
2019-09-05 11:15:04 +0200

    Enhances logging configuration

--- common/src/zdl_lex_common/log.clj
+++ common/src/zdl_lex_common/log.clj
@@ -1,6 +1,7 @@
 (ns zdl-lex-common.log
   (:require [taoensso.timbre :as timbre]
-            [environ.core :refer [env]])
+            [environ.core :refer [env]]
+            [clojure.string :as str])
   (:import org.slf4j.bridge.SLF4JBridgeHandler))
 
 (defn configure-slf4j-bridge []
@@ -9,7 +10,7 @@
 
 (defn configure-timbre []
   (timbre/handle-uncaught-jvm-exceptions!)
-  (timbre/set-level! (env :zdl-lex-log-level :info))
+  (timbre/set-level! (-> (env :zdl-lex-log-level "INFO") str/lower-case keyword))
   (timbre/merge-config!
    {:ns-blacklist ["clj-soap.client"
                    "httpclient.*"
@@ -20,3 +21,6 @@
                    "org.eclipse.jetty.*"
                    "org.eclipse.jgit.*"]}))
 
+(defn configure []
+  (configure-slf4j-bridge)
+  (configure-timbre))
