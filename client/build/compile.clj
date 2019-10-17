e973fd1d5671c5562ca1c231f3517a5fd55212f2
Gregor Middell <gregor@middell.net>
2019-10-17 21:42:39 +0200

    Fixes AOT issues

---
+++ client/build/compile.clj
@@ -0,0 +1,13 @@
+(ns compile)
+
+(def compiled-ns
+  ['zdl-lex-client.oxygen.extension
+   'zdl-lex-client.oxygen.plugin
+   'zdl-lex-client.oxygen.url-handler])
+
+(defn -main [& args]
+  (binding [*compile-path* "classes"
+            *compiler-options* {:disable-locals-clearing true
+                                :elide-meta []
+                                :direct-linking false}]
+    (doseq [ns compiled-ns] (compile ns))))
--- server/build/package.clj
+++ server/build/package.clj
@@ -1,16 +1,9 @@
-(ns zdl-lex-server.build
-  (:require [me.raynes.fs :as fs]
-            [uberdeps.api :as uberdeps]
-            [zdl-lex-common.log :as log]))
+(ns package
+  (:require [uberdeps.api :as uberdeps]))
 
 (def jar-path
   "../ansible/files/api/org.zdl.lex.server-standalone.jar")
 
 (defn -main [& args]
-  (log/configure)
   (let [deps (-> "deps.edn" slurp read-string)]
     (uberdeps/package deps jar-path {:aliases #{:prod}})))
-
-(comment (-main))
-
-
