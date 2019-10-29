c616c73fa5c9683df58c50474f050e1c71f96d69
Gregor Middell <gregor@middell.net>
2019-10-29 17:23:12 +0100

    Adds some documentation

--- common/src/zdl_lex_common/xml_schema.clj
+++ common/src/zdl_lex_common/xml_schema.clj
@@ -1,18 +1,18 @@
 (ns zdl-lex-common.xml-schema
-  (:require [zdl-lex-common.util :refer [file]]
-            [zdl-lex-common.xml :as xml]
-            [clojure.string :as str]
-            [clojure.data.zip :as dz]
+  "RELAX NG schema parser, based on Kohsuke Kawaguchi's RNG-OM."
+  (:require [clojure.data.zip :as dz]
             [clojure.data.zip.xml :as zx]
+            [clojure.string :as str]
             [clojure.zip :as zip]
-            [taoensso.timbre :as timbre])
+            [zdl-lex-common.util :refer [file]]
+            [zdl-lex-common.xml :as xml])
   (:import org.kohsuke.rngom.ast.util.CheckingSchemaBuilder
            org.kohsuke.rngom.digested.DSchemaBuilderImpl
            org.kohsuke.rngom.parse.xml.SAXParseable
            org.xml.sax.helpers.DefaultHandler))
 
-(defn- parse-name
-  "Converts name classes.
+(defn- name*
+  "Parses a name class.
 
   Beware: Does not handle exception classes!"
   [nc]
@@ -28,6 +28,7 @@
     (persistent! names)))
 
 (defn- parse*
+  "Parses a pattern."
   [p]
   (let [clz (class p)
         type (-> clz .getSimpleName
@@ -37,7 +38,7 @@
         container? (isa? clz org.kohsuke.rngom.digested.DContainerPattern)
         xml-token? (isa? clz org.kohsuke.rngom.digested.DXmlTokenPattern)]
     (merge {:tag type}
-           (when xml-token? {:name (parse-name (.getName p))})
+           (when xml-token? {:name (name* (.getName p))})
            (when unary? {:content (list (parse* (.getChild p)))})
            (when container? {:content (map parse* p)})
            (condp = type
@@ -57,31 +58,36 @@
                                       (.getParams p)))}
              {}))))
 
-(def ^:private sax-error-handler
-  (proxy [DefaultHandler] []
-    (error
-      [e]
-      (throw e))))
-
 (defn parse
+  "Parses a schema in RELAX NG XML format.
+
+   The resulting tree structure ressembles `clojure.xml` parse trees and
+   can be processed with fns for such trees accordingly."
   [rng]
-  (let [schema-builder (CheckingSchemaBuilder. (DSchemaBuilderImpl.) sax-error-handler)
-        schema (SAXParseable. (xml/->input-source rng) sax-error-handler)]
+  (let [eh (proxy [DefaultHandler] [] (error [e] (throw e)))
+        schema-builder (CheckingSchemaBuilder. (DSchemaBuilderImpl.) eh)
+        schema (SAXParseable. (xml/->input-source rng) eh)]
     (parse* (.. schema (parse schema-builder)))))
 
 (defn name=
+  "Predicate for zipper locations, matching a given name with a name class."
   ([name]
    (partial name= name))
   ([name loc]
    (some-> loc zip/node :name (contains? name))))
 
-(def ^:private >> dz/descendants)
+(def ^:private >>
+  "Shortcut notation for descendants of a zipper location."
+  dz/descendants)
 
-(defn collect-values [& path]
+(defn collect-values
+  "Collects a sorted set of values in a subtree selected by the given path."
+  [& path]
   (->> (apply zx/xml-> (concat path [>> :value (zx/attr :value)]))
        (into (sorted-set))))
 
 (defn pdef?
+  "Predicate for pattern definitions, matched by their name."
   ([name]
    (partial pdef? name))
   ([name loc]
