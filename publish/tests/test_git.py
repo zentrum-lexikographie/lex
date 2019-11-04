7dab5a0ada6513e24a10c3eb5f5874b6de2b44f6
Gregor Middell <gregor@middell.net>
2019-11-04 16:48:44 +0100

    Adds gitpython

--- publish/tests/test_git.py
+++ publish/tests/test_git.py
@@ -1,12 +1,19 @@
 from pytest import fixture
 from pathlib import Path
 
+from git import Repo
+
 
 @fixture
-def articles_dir():
-    p = (Path(__file__).parent) / '..' / '..' / 'data' / 'git' / 'articles'
+def git_dir():
+    p = (Path(__file__).parent) / '..' / '..' / 'data' / 'qa'
     return p.resolve()
 
 
-def test_red_2(articles_dir):
-    assert articles_dir.is_dir()
+@fixture
+def articles_dir(git_dir):
+    return (git_dir / 'articles').resolve()
+
+
+def test_red_2(git_dir):
+    assert 'zdl-lex-server' in Repo(git_dir.as_posix()).heads
