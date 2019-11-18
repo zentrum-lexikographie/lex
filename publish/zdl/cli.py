eabfa8625ed10a4b133c96445a950c9f767f3085
Gregor Middell <gregor@middell.net>
2019-11-18 17:22:37 +0100

    Refactors Python CLI code

--- publish/zdl/cli.py
+++ publish/zdl/cli.py
@@ -1,52 +1,77 @@
 from pathlib import Path
 import click
 
+import zdl.lex
 import zdl.metadata
 import zdl.mysql
 import zdl.qa
 
 
-def article_files(articles_dir):
-    return zdl.article.files(Path(articles_dir))
+def home(ctx):
+    return ctx.obj['home']
 
 
-def article_progress(articles_dir, label):
-    num_articles = sum([1 for f in article_files(articles_dir)])
+def article_files(ctx):
+    return zdl.article.files(home(ctx))
+
+
+def article_progress(ctx, label):
+    num_articles = sum([1 for f in article_files(ctx)])
     return click.progressbar(
-        article_files(articles_dir),
+        article_files(ctx),
         length=num_articles,
         width=0, label=label
     )
 
 
 @click.group()
-@click.argument('articles-dir',
-                required=True,
-                type=click.Path(exists=True, file_okay=False, resolve_path=True))
+@click.option('--server-base',
+              envvar='ZDL_LEX_SERVER_BASE',
+              default='https://lex.dwds.de/')
+@click.option('--server-user',
+              envvar='ZDL_LEX_SERVER_USER')
+@click.option('--server-password',
+              envvar='ZDL_LEX_SERVER_PASSWORD')
+@click.option('--git-origin',
+              envvar='ZDL_LEX_GIT_ORIGIN',
+              default='git@lex.dwds.de:lex.git')
+@click.option('--git-branch',
+              envvar='ZDL_LEX_GIT_BRANCH',
+              default='zdl-lex-server')
+@click.option('--home',
+              envvar='ZDL_LEX_HOME',
+              required=True,
+              type=click.Path(exists=True, file_okay=False, resolve_path=True))
 @click.pass_context
-def cli(ctx, articles_dir):
+def cli(ctx,
+        server_base, server_user, server_password,
+        git_origin, git_branch,
+        home):
     ctx.ensure_object(dict)
-    ctx.obj['articles_dir'] = articles_dir
+    ctx.obj['server'] = zdl.lex.Server(
+        server_base,
+        (server_user, server_password) if server_user else None
+    )
+    ctx.obj['home'] = Path(home)
 
 
 @cli.command()
-@click.pass_obj
-def metadata(obj):
-    articles_dir = obj['articles_dir']
-    zdl.metadata.read(Path(articles_dir), article_files(articles_dir))
+@click.pass_context
+def metadata(ctx):
+    zdl.metadata.read(home(ctx), article_files(ctx))
 
 
 @cli.command()
-@click.pass_obj
-def qa(obj):
-    with article_progress(obj['articles_dir'], 'Red-1 -> Red-2') as articles:
+@click.pass_context
+def qa(ctx):
+    with article_progress(ctx, 'Red-1 -> Red-2') as articles:
         zdl.qa.perform(articles)
 
 
 @cli.command()
-@click.pass_obj
-def wb2db(obj):
-    with article_progress(obj['articles_dir'], 'Red-f -> MySQL') as articles:
+@click.pass_context
+def wb2db(ctx):
+    with article_progress(ctx, 'Red-f -> MySQL') as articles:
         zdl.mysql.wb2db(articles)
 
 
