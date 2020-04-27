5ca490b9aaea84705207ad5d2a1bab54014ce05a
Gregor Middell <gregor@middell.net>
2020-04-27 11:00:47 +0200

    Tests random generation of article subsets

---
+++ server/test/zdl/lex/server/gen.clj
@@ -0,0 +1,11 @@
+(ns zdl.lex.server.gen
+  (:require  [clojure.test :as t]
+             [zdl.lex.article.fs :as afs]
+             [clojure.spec.gen.alpha :as gen]))
+
+(comment
+  (gen/generate
+   (gen/set
+    (gen/elements (afs/files "/home/gregor/repositories/zdl-wb"))
+    {:min-elements 10 :max-elements 1000}))
+  (time (count (afs/files "/home/gregor/repositories/zdl-wb"))))
