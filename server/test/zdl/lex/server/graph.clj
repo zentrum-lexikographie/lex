a5a8ac865a003b7c27cd05c1c527fe198318cb4e
Gregor Middell <gregor@middell.net>
2020-04-29 08:30:29 +0200

    Adds test fixture for graph db

--- server/test/zdl/lex/server/graph.clj
+++ server/test/zdl/lex/server/graph.clj
@@ -1,27 +1,45 @@
 (ns zdl.lex.server.graph
-  (:require  [clojure.test :refer :all]
-             [zdl.lex.data :as data]
-             [zdl.lex.fs :refer [path]]
-             [clojurewerkz.ogre.core :as g])
-  (:import java.util.Properties
-           org.apache.commons.configuration.MapConfiguration
+  (:require [clojure.test :refer :all]
+            [clojurewerkz.ogre.core :as g]
+            [mount.core :as mount :refer [defstate]]
+            [zdl.lex.data :as data]
+            [zdl.lex.fs :refer [delete! path]])
+  (:import org.apache.commons.configuration.MapConfiguration
            org.apache.tinkerpop.gremlin.structure.util.GraphFactory))
 
-(def config
-  (delay
-    (MapConfiguration.
-     {"gremlin.graph" "org.janusgraph.core.JanusGraphFactory"
-      "storage.backend" "berkeleyje"
-      "storage.directory" (path (data/dir "janus-db"))
-      "index.lex.backend" "lucene"
-      "index.lex.directory" (path (data/dir "janus-index"))})))
+(def db-dir
+  (delay (data/dir "janus-db")))
 
-(comment
-  (with-open [g (GraphFactory/open @config)]
-    (.. g (tx) (open))
-    (try
-      (let [g (g/traversal g)]
-        (g/traverse g g/V (g/addV "test") (g/next!))
-        (g/traverse g g/V (g/addV "test") (g/next!))
-        (g/traverse g g/V (g/count) (g/next!)))
-      (finally (.. g (tx) (commit))))))
+(def index-dir
+  (delay (data/dir "janus-index")))
+
+(defstate graph
+  :start (->>
+          (MapConfiguration.
+           {"gremlin.graph" "org.janusgraph.core.JanusGraphFactory"
+            "storage.backend" "berkeleyje"
+            "storage.directory" (path @db-dir)
+            "index.lex.backend" "lucene"
+            "index.lex.directory" (path @index-dir)})
+          (GraphFactory/open))
+  :stop (.close graph))
+
+(defn delete-graph!
+  []
+  (delete! @index-dir)
+  (delete! @db-dir))
+
+(defn graph-fixture
+  [f]
+  (try
+    (mount/start #'graph)
+    (f)
+    (finally
+      (mount/stop #'graph))))
+
+(use-fixtures :once graph-fixture)
+
+(deftest graph-setup
+  (is (let [g (g/traversal graph)]
+        (g/traverse g (g/addV "test") (g/next!))
+        (g/traverse g (g/addV "test") (g/next!)))))
