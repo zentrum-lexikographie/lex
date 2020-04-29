b232b2cc599c9c115bfdd2b927b0cd218211eea7
Gregor Middell <gregor@middell.net>
2020-04-29 10:27:38 +0200

    Adds graph transaction handling

--- server/test/zdl/lex/server/graph.clj
+++ server/test/zdl/lex/server/graph.clj
@@ -4,8 +4,13 @@
             [mount.core :as mount :refer [defstate]]
             [zdl.lex.data :as data]
             [zdl.lex.fs :refer [delete! path]])
-  (:import org.apache.commons.configuration.MapConfiguration
-           org.apache.tinkerpop.gremlin.structure.util.GraphFactory))
+  (:import java.util.Map
+           org.apache.commons.configuration.MapConfiguration
+           org.apache.tinkerpop.gremlin.structure.util.GraphFactory
+           org.janusgraph.core.JanusGraphTransaction
+           org.janusgraph.graphdb.database.StandardJanusGraph))
+
+(set! *warn-on-reflection* true)
 
 (def db-dir
   (delay (data/dir "janus-db")))
@@ -14,20 +19,43 @@
   (delay (data/dir "janus-index")))
 
 (defstate graph
-  :start (->>
-          (MapConfiguration.
-           {"gremlin.graph" "org.janusgraph.core.JanusGraphFactory"
-            "storage.backend" "berkeleyje"
-            "storage.directory" (path @db-dir)
-            "index.lex.backend" "lucene"
-            "index.lex.directory" (path @index-dir)})
-          (GraphFactory/open))
-  :stop (.close graph))
+  :start (let [^Map config
+               {"gremlin.graph" "org.janusgraph.core.JanusGraphFactory"
+                "storage.backend" "berkeleyje"
+                "storage.directory" (path @db-dir)
+                "index.lex.backend" "lucene"
+                "index.lex.directory" (path @index-dir)}]
+           (GraphFactory/open (MapConfiguration. config)))
+  :stop (.close ^StandardJanusGraph graph))
 
 (defn delete-graph!
   []
   (delete! @index-dir)
   (delete! @db-dir))
+
+(defn ^JanusGraphTransaction new-tx!
+  [^StandardJanusGraph graph]
+  (.newTransaction graph))
+
+(defn commit-tx!
+  [^JanusGraphTransaction tx]
+  (.commit tx))
+
+(defn rollback-tx!
+  [^JanusGraphTransaction tx]
+  (.rollback tx))
+
+(defmacro with-tx
+  [binding & body]
+  `(let [tx# (new-tx! ~(second binding))
+         ~(first binding) (g/traversal tx#)]
+     (try
+       (let [result# (do ~@body)]
+         (commit-tx! tx#)
+         result#)
+       (catch Throwable t#
+         (rollback-tx! tx#)
+         (throw t#)))))
 
 (defn graph-fixture
   [f]
@@ -35,11 +63,14 @@
     (mount/start #'graph)
     (f)
     (finally
-      (mount/stop #'graph))))
+      (mount/stop #'graph)
+      (delete-graph!))))
 
 (use-fixtures :once graph-fixture)
 
 (deftest graph-setup
-  (is (let [g (g/traversal graph)]
-        (g/traverse g (g/addV "test") (g/next!))
-        (g/traverse g (g/addV "test") (g/next!)))))
+  (is
+   (with-tx [g graph]
+     (println (g/traverse g (g/V) (g/count) (g/next!)))
+     (g/traverse g (g/addV "test") (g/next!))
+     (g/traverse g (g/addV "test") (g/next!)))))
