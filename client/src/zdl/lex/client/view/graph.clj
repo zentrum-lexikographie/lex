d846c29389c1ba885c6d3a3ee8e1e0d948235462
Gregor Middell <gregor@middell.net>
2021-06-08 20:56:19 +0200

    Graph display updates properly

--- client/src/zdl/lex/client/view/graph.clj
+++ client/src/zdl/lex/client/view/graph.clj
@@ -5,48 +5,47 @@
             [zdl.lex.client.bus :as bus])
   (:import com.google.common.base.Supplier
            edu.uci.ics.jung.algorithms.generators.random.ErdosRenyiGenerator
-           [edu.uci.ics.jung.algorithms.layout FRLayout ISOMLayout]
+           [edu.uci.ics.jung.algorithms.layout FRLayout ISOMLayout SpringLayout]
            [edu.uci.ics.jung.graph DirectedSparseGraph DirectedSparseMultigraph UndirectedSparseGraph]
            edu.uci.ics.jung.graph.util.TestGraphs
            [edu.uci.ics.jung.visualization GraphZoomScrollPane Layer VisualizationViewer]
            edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse))
 
-(defn supplier
-  [f]
-  (proxy [Supplier] []
-    (get [] (f))))
-
 (def graph
   (atom nil))
 
-(def layout
-  (FRLayout. (DirectedSparseMultigraph.)))
-
-(def viewer
-  (let [graph-mouse (DefaultModalGraphMouse.)]
-    (doto (VisualizationViewer. layout)
-      (.setBackground java.awt.Color/WHITE)
-      (.setGraphMouse graph-mouse)
-      (.addKeyListener (.getModeKeyListener graph-mouse)))))
-
 (def panel
-  (GraphZoomScrollPane. viewer))
+  (ui/border-panel :center (ui/label :id :graph-view)))
 
-(defn render-graph!
-  [g]
-  (let [{:keys [nodes edges] :as g} {:nodes (into {} (g :nodes))
-                                     :edges (vec (g :edges))}
-        model                       (DirectedSparseMultigraph.)]
-    (reset! graph g)
+(defn graph->model
+  [{:keys [nodes edges] :as g}]
+  (reset! graph g)
+  (let [model (DirectedSparseMultigraph.)]
     (doseq [node (keys nodes)]
       (.addVertex model node))
     (doseq [[id [src target]] (map-indexed list edges)]
       (.addEdge model id src target))
-    (log/info model)
-    (.setGraph layout model))
-  (let [transformer (.. viewer getRenderContext getMultiLayerTransformer)]
+    model))
+
+(defn render-graph!
+  [g]
+  (let [g           {:nodes (into {} (g :nodes))
+                     :edges (vec (g :edges))}
+        model       (graph->model g)
+        layout      (ISOMLayout. model)
+        graph-mouse (DefaultModalGraphMouse.)
+        viewer      (doto (VisualizationViewer. layout)
+                      (.setBackground java.awt.Color/WHITE)
+                      (.setGraphMouse graph-mouse)
+                      (.addKeyListener
+                       (.getModeKeyListener graph-mouse)))
+        scroll-pane (ui/config! (GraphZoomScrollPane. viewer) :id :graph-view)]
+    (ui/invoke-soon
+     (ui/replace! panel (ui/select panel [:#graph-view]) scroll-pane))))
+
+#_(let [transformer (.. viewer getRenderContext getMultiLayerTransformer)]
     (.. transformer (getTransformer Layer/LAYOUT) (setToIdentity))
-    (.. transformer (getTransformer Layer/VIEW) (setToIdentity))))
+    (.. transformer (getTransformer Layer/VIEW) (setToIdentity)))
 
 (defstate graph-renderer
   :start (bus/listen #{:graph} (fn [_ [id graph]] (render-graph! graph)))
