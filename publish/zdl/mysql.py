a5e6d262aee77a11ee808286b730f89bfc27cce0
Axel Herold <herold@bbaw.de>
2021-07-02 12:59:11 +0200

    Fix https://git.zdl.org/zdl/lex/issues/24
    For mysql (and decendants) utf8 != utf8mb4 ... (utf8 encoding only supports up to three byte encoded codepoints)

--- publish/zdl/mysql.py
+++ publish/zdl/mysql.py
@@ -10,6 +10,7 @@ import zdl.util
 _version = datetime.datetime.now().strftime('%Y-%m-%d')
 _ddl_statements = [
     'DROP FUNCTION IF EXISTS dictionary_version',
+    'SET NAMES "utf8mb4" COLLATE "utf8mb4_unicode_ci"',
     ('CREATE FUNCTION dictionary_version () RETURNS CHAR(%i) RETURN "%s"' %
      (len(_version), _version)),
     'DROP TABLE IF EXISTS article',
@@ -23,32 +24,32 @@ _ddl_statements = [
          source CHAR(32) DEFAULT "DWDS",
          recommendation TINYINT(1) default 0,
          date DATE NOT NULL,
-         xml MEDIUMTEXT COLLATE utf8_unicode_ci NOT NULL,
+         xml MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,
          PRIMARY KEY (id),
          KEY status (status)
-       ) ENGINE=MyISAM DEFAULT CHARSET=utf8''',
+       ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS lemma (
          id INT(11) NOT NULL,
-         lemma VARCHAR(200) COLLATE utf8_unicode_ci NOT NULL,
+         lemma VARCHAR(200) COLLATE utf8mb4_unicode_ci NOT NULL,
          hidx TINYINT(2) DEFAULT NULL,
          type VARCHAR(10),
          article_id INT(11) NOT NULL,
          PRIMARY KEY (id),
          KEY lemma (lemma),
          KEY article_id(article_id, lemma)
-       ) ENGINE=MyISAM DEFAULT CHARSET=utf8''',
+       ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS token (
-         token VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+         token VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
          lemma_id INT(11) NOT NULL,
          KEY token (token)
-       ) ENGINE=MyISAM DEFAULT CHARSET=utf8''',
+       ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS relation (
          article1 INT(11) NOT NULL,
          article2 INT(11) NOT NULL,
-         type VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
+         type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
          KEY article1 (article1, article2, type),
          KEY article2 (article2, article1, type)
-       ) ENGINE=MyISAM DEFAULT CHARSET=utf8'''
+       ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4'''
 ]
 
 
@@ -163,7 +164,7 @@ def import_articles(records, db_url, echo=False):
 )
 @click.command()
 def cli(article_dirs, mysql_host, mysql_db, mysql_user, mysql_password):
-    mysql_url = 'mysql+pymysql://%s:%s@%s/%s' % (
+    mysql_url = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8mb4' % (
         mysql_user, mysql_password, mysql_host, mysql_db
     )
     with zdl.util.article_progress(article_dirs, 'Red-f -> MySQL') as articles:
