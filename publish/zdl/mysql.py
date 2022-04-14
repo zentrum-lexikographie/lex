ab9dcd16da132a9112c3cc717abe0bd2b5d6b9c0
Axel Herold <herold@bbaw.de>
2022-04-14 10:50:33 +0200

    Change collation

--- publish/zdl/mysql.py
+++ publish/zdl/mysql.py
@@ -10,7 +10,7 @@ import zdl.util
 _version = datetime.datetime.now().strftime('%Y-%m-%d')
 _ddl_statements = [
     'DROP FUNCTION IF EXISTS dictionary_version',
-    'SET NAMES "utf8mb4" COLLATE "utf8mb4_unicode_ci"',
+    'SET NAMES "utf8mb4" COLLATE utf8mb4_bin',
     ('CREATE FUNCTION dictionary_version () RETURNS CHAR(%i) RETURN "%s"' %
      (len(_version), _version)),
     'DROP TABLE IF EXISTS article',
@@ -24,13 +24,13 @@ _ddl_statements = [
          source CHAR(32) DEFAULT "DWDS",
          recommendation TINYINT(1) default 0,
          date DATE NOT NULL,
-         xml MEDIUMTEXT COLLATE utf8mb4_unicode_ci NOT NULL,
+         xml MEDIUMTEXT COLLATE utf8mb4_bin NOT NULL,
          PRIMARY KEY (id),
          KEY status (status)
        ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS lemma (
          id INT(11) NOT NULL,
-         lemma VARCHAR(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+         lemma VARCHAR(200) COLLATE utf8mb4_bin NOT NULL,
          hidx TINYINT(2) DEFAULT NULL,
          type VARCHAR(10),
          article_id INT(11) NOT NULL,
@@ -39,14 +39,14 @@ _ddl_statements = [
          KEY article_id(article_id, lemma)
        ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS token (
-         token VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+         token VARCHAR(50) COLLATE utf8mb4_bin NOT NULL,
          lemma_id INT(11) NOT NULL,
          KEY token (token)
        ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4''',
     '''CREATE TABLE IF NOT EXISTS relation (
          article1 INT(11) NOT NULL,
          article2 INT(11) NOT NULL,
-         type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+         type VARCHAR(20) COLLATE utf8mb4_bin NOT NULL,
          KEY article1 (article1, article2, type),
          KEY article2 (article2, article1, type)
        ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4'''
