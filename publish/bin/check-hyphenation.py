84aa7614bfc9ed02b63fe4f6ba18408a3e397496
Axel Herold <herold@bbaw.de>
2026-02-27 12:17:58 +0100

    Adds formal checks for hyphenation (@Trennung)

---
+++ publish/bin/check-hyphenation.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+
+import argparse
+import lxml.etree as et
+from Wb import Wb
+
+argument_parser = argparse.ArgumentParser(description='Hyphenation checks.')
+argument_parser.add_argument('-p', '--path',
+        action='store_true',
+        default=False,
+        help='show article URIs instead of headword and snippet')
+argument_parser.add_argument('-s', '--subset',
+        choices=('all', 'recent'),
+        default='recent',
+        help='only check a subset of the entries (default: recent)')
+arguments = argument_parser.parse_args()
+
+wb = Wb()
+wdg = set( [l.strip() for l in open('share/WDG-lemmas.txt', encoding='utf-8') ] )
+duden = set( [l.strip() for l in open('share/Duden-lemmas.txt', encoding='utf-8') ] )
+
+for entry, path in wb:
+    for orth in entry.findall('.//%(Schreibung)s' % wb.TAGS):
+        hyphenation = orth.get('Trennung')
+        if orth.get('Typ') not in (None, 'U_CH', 'U_AT') and hyphenation is not None:
+            wb.report(entry, path, 'Hyphenation on marked headword', verbose=not(arguments.path))
+        if hyphenation is None:
+            pass
+        elif wb.text(orth).replace('-', '') != hyphenation.replace('-', ''):
+            wb.report(entry, path, f'Mismatched string after hyphenation: "{wb.text(orth)}" vs. "{hyphenation}"', verbose=not(arguments.path))
+
+
+if arguments.path:
+    print()
