4246da24369a547cb2218b9d770db593f53219d5
Axel Herold <herold@bbaw.de>
2026-07-07 08:40:32 +0200

    Adds sanity checks for //Artikel/@Erstfassung

--- publish/bin/check-first-recording.py
+++ publish/bin/check-first-recording.py
@@ -12,14 +12,19 @@ args = ap.parse_args()
 
 erstfassung = collections.defaultdict(set)
 erstellungsdatum = collections.defaultdict(set)
+zeitstempel = collections.defaultdict(set)
 
 for entry, path in wb:
     h = wb.get_headwords(entry)[0]
     erstfassung[h].add(entry.get('Erstfassung'))
     erstellungsdatum[h].add(entry.get('Erstellungsdatum'))
+    zeitstempel[h].add(entry.get('Zeitstempel'))
 
 for headword in filter(lambda x: len(erstfassung[x]) > 1, erstfassung.keys()):
     print(f'{headword}\tinconsistent @Erstfassung: {" vs. ".join(sorted(erstfassung[headword]))}')
 
-for headword in filter(lambda x: len(erstellungsdatum[x]) > 1, erstellungsdatum.keys()):
-    print(f'{headword}\tinconsistent @Erstellungsdatum: {" vs. ".join(sorted(erstellungsdatum[headword]))}')
+for headword, dates in erstellungsdatum.items():
+    if len(dates) > 1:
+        print(f'{headword}\tinconsistent @Erstellungsdatum: {" vs. ".join(sorted(dates))}')
+    #if 'DWDS' in erstfassung[headword] and not min(zeitstempel[headword]) == min(dates):
+    #    print(f'{headword}\tno entry with @Zeitstempel == @Erstellungsdatum == {min(dates)}')
