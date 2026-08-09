704beca0e563613eca51935251da884509ca98d1
Gregor Middell <gregor@middell.net>
2026-08-09 22:40:33 +0200

    Import MantisBT issues via SOAP-API
    Replacing the import via access to the MySQL database undelying
    Mantis, this couples the ZDL's DWS more loosely and allows it to be
    shifted to another data centre.

--- src/zdl/lex/issue.clj
+++ src/zdl/lex/issue.clj
@@ -1,66 +1,84 @@
 (ns zdl.lex.issue
   (:require
    [clojure.string :as str]
+   [gremid.xml :as gx]
    [lambdaisland.uri :as uri]
-   [next.jdbc :as jdbc]
-   [next.jdbc.result-set :as jdbc.result-set]
+   [medley.core :refer [distinct-by]]
+   [org.httpkit.client :as hc]
    [tick.core :as t]
    [zdl.lex.env :refer [getenv]]
    [zdl.lex.index :as index]
    [zdl.lex.metrics :as metrics]))
 
-(def db
-  {:dbtype   "mysql"
-   :host     (getenv "MANTIS_DB_HOST" "localhost")
-   :port     (parse-long (getenv "MANTIS_DB_PORT" "3306"))
-   :dbname   (getenv "MANTIS_DB_NAME" "mantis_bugtracker")
-   :username (getenv "MANTIS_DB_USER" "mantis")
-   :password (getenv "MANTIS_DB_PASSWORD" "mantis")})
+(def api-url
+  "https://mantis.dwds.de/mantis/api/soap/mantisconnect.php")
 
-(defn issue-id->uri
-  [id]
-  (str (uri/->URI "mantis" nil nil nil nil (str id) nil nil)))
+(def api-soap-action
+  (str api-url "/mc_project_get_issues"))
 
-(def status-descs
-  {"10" "new",
-   "20" "feedback",
-   "30" "acknowledged",
-   "40" "confirmed",
-   "50" "assigned",
-   "80" "resolved",
-   "90" "closed"})
+(def api-user
+  (getenv "MANTIS_API_USER" "dwdsweb"))
 
-(def severity-descs
-  {"10" "feature",
-   "20" "trivial",
-   "30" "text",
-   "40" "tweak",
-   "50" "minor",
-   "60" "major",
-   "70" "crash",
-   "80" "block"})
+(def api-password
+  (getenv "MANTIS_API_PASSWORD"))
 
-(def resolution-descs
-  {"50" "not fixable",
-   "60" "duplicate",
-   "30" "reopened",
-   "80" "suspended",
-   "20" "fixed",
-   "90" "won't fix",
-   "70" "no change required",
-   "10" "open",
-   "40" "unable to reproduce"})
+(defn xml-property
+  [node property]
+  (some->> node (gx/element property) (gx/text)))
 
+(defn xml-ref-property
+  [node property ref-prop]
+  (some->> node (gx/element property) (gx/element ref-prop) (gx/text)))
 
-(defn parse-issue
-  [{:keys [id summary status severity resolution updated] :as issue}]
-  (assoc issue
-         :id (issue-id->uri id)
-         :updated (some-> updated (* 1000) (t/instant) str)
-         :form (some-> summary (str/split #" --") first)
-         :status (some-> status str status-descs)
-         :severity (some-> severity str severity-descs)
-         :resolution (some-> resolution str resolution-descs)))
+(defn xml->issue
+  [item-xml]
+  (let [summary (xml-property item-xml :summary)]
+    {:id         (str (uri/->URI "mantis" nil nil nil nil
+                                 (xml-property item-xml :id) nil nil))
+     :summary    summary
+     :form       (some-> summary (str/split #" --") first)
+     :updated    (some-> (xml-property item-xml :last_updated)
+                         (t/offset-date-time) (t/instant) (str))
+     :category   (xml-ref-property item-xml :category :name)
+     :status     (xml-ref-property item-xml :status :name)
+     :severity   (xml-ref-property item-xml :severity :name)
+     :reporter   (xml-ref-property item-xml :reporter :real_name)
+     :handler    (xml-ref-property item-xml :handler :real_name)
+     :resolution (xml-ref-property item-xml :resolution :name)}))
+
+(defn request-issues
+  [page]
+  (let [soap-msg (->>
+                  [:SOAP-ENV:Envelope
+                   {:xmlns:SOAP-ENV         "http://schemas.xmlsoap.org/soap/envelope/"
+                    :xmlns:soap             "http://schemas.xmlsoap.org/wsdl/soap/"
+                    :xmlns:mantis           "http://futureware.biz/mantisconnect"
+                    :xmlns:xsd              "http://www.w3.org/2001/XMLSchema"
+                    :xmlns:xsi              "http://www.w3.org/2001/XMLSchema-instance"
+                    :SOAP-ENV:encodingStyle "http://schemas.xmlsoap.org/soap/encoding/"}
+                   [:SOAP-ENV:Body
+                    [:mantis:mc_project_get_issues
+                     [:username {:xsi:type "xsd:string"} api-user]
+                     [:password {:xsi:type "xsd:string"} api-password]
+                     [:project_id {:xsi:type "xsd:integer"} "5"]
+                     [:page_number {:xsi:type "xsd:integer"} (str page)]
+                     [:per_page {:xsi:type "xsd:integer"} "500"]]]]
+                  (gx/sexp->node) (gx/write-node *out*) (with-out-str))
+        resp     @(hc/request {:method  :post
+                               :url     api-url
+                               :headers {"Content-Type" "text/xml"
+                                         "SOAPAction"   api-soap-action}
+                               :body    soap-msg})]
+    (when (resp :error) (throw (resp :error)))
+    (when (not= (resp :status) 200) (throw (ex-info "Mantis API error" resp)))
+    (some->> resp :body gx/read-events gx/events->node
+             (gx/element :return) :content (map xml->issue))))
+
+(defn issues
+  ([]
+   (distinct-by :id (issues 1)))
+  ([page]
+   (some-> (request-issues page) (seq) (lazy-cat (issues (inc page))))))
 
 (def sync-timer
   (metrics/timer "mantis.sync"))
@@ -70,26 +88,8 @@
   (with-open [_ (metrics/timed! sync-timer)]
    (let [threshold (System/currentTimeMillis)]
      (transduce
-      (comp (map parse-issue) (filter :form) (map index/issue->doc)
-            (partition-all 10000))
+      (comp (filter :form) (map index/issue->doc) (partition-all 10000))
       (completing (fn [n batch] (index/add! batch) (+ n (count batch))))
       0
-      (jdbc/plan
-       db
-       "SELECT
-          bug.id as id,
-          bug.summary as summary,
-          bug.last_updated as updated,
-          category.name as category,
-          bug.status as status,
-          bug.severity as severity,
-          reporter.realname as reporter,
-          handler.realname as handler,
-          bug.resolution as resolution
-        FROM mantis_bug_table bug
-        LEFT JOIN mantis_user_table reporter ON bug.reporter_id = reporter.id
-        LEFT JOIN mantis_user_table handler ON bug.handler_id = handler.id
-        LEFT JOIN mantis_category_table category ON bug.category_id = category.id
-        WHERE bug.project_id = 5"
-       {:builder-fn jdbc.result-set/as-unqualified-kebab-maps}))
+      (issues))
      (index/purge! "issue" threshold))))
